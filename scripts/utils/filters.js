203a1e59906e9aac24419f7b1bcdf64626fa04b8
Julie V <julievitse91@gmail.com>
2022-06-11 04:23:04 +0200

    html/ css search & filters

---
+++ scripts/utils/filters.js
@@ -0,0 +1,4 @@
+const filterBtn = document.querySelectorAll('.search__filters__filter__wrapper__input__btn');
+const filterList = document.querySelectorAll('.search__filters__filter__wrapper__list');
+const filter = document.querySelectorAll('.search__filters__filter');
+const filterName = document.querySelectorAll('.search__filters__filter__wrapper__input__label')
