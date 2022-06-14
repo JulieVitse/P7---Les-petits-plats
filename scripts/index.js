25000619bdbdaf0aefee9dd4b673ac6ca55ca3a9
Julie V <julievitse91@gmail.com>
2022-06-14 08:32:06 +0200

    added filter lists

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,60 @@
+let ustensilsArray = [];
+let applianceArray = [];
+let ingredientsArray = [];
+
+const tags = ["ingredients", "appareils", "ustensiles"];
+
+const filters = {
+    ingredients: new Filter(document.querySelector('[data-filter="ingredients"]')),
+    appareils: new Filter(document.querySelector('[data-filter="appareils"]')),
+    ustensiles: new Filter(document.querySelector('[data-filter="ustensiles"]'))
+};
+
+for(const [, filter] of Object.entries(filters)) {
+    console.log(filter.name)
+}
+// créé arrays pour listes de filtre
+recipes.map((recipe) => {
+    recipe.ustensils.map((ustensil) => {
+        ustensilsArray.push(ustensil.toLowerCase());
+        ustensilsArray = [...new Set(ustensilsArray)].sort();
+    });
+    let ingredientList = recipe.ingredients.map(a => a.ingredient.toLowerCase());
+    ingredientsArray.push(...ingredientList);
+    ingredientsArray = [...new Set(ingredientsArray)].sort();
+ 
+    applianceArray.push(recipe.appliance.toLowerCase());
+    applianceArray = [...new Set(applianceArray)].sort();
+})
+
+function createLists () {
+    ingredientsArray.map((ingredient) => {
+        const li = new ListElements(ingredient).displayItem();
+        filters.ingredients.list.append(li);
+    });
+    applianceArray.map((appliance) => {
+        const li = new ListElements(appliance).displayItem();
+        filters.appareils.list.append(li);
+    });
+    ustensilsArray.map((ustensil) => {
+        const li = new ListElements(ustensil).displayItem();
+        filters.ustensiles.list.append(li);
+    });
+}
+
+//display recettes
 async function displayData() {
     const container = document.querySelector('.recipes');
 
     recipes.forEach((recipe) => {
-        container.appendChild(new Recipe(recipe).displayRecipe());
+        const recipeCard = new Recipe(recipe).displayRecipe();
+        container.appendChild(recipeCard);
     })
 }
 
-async function init (){
+async function init () {
     await displayData();
+    createLists();
 }
 
 init();
--- scripts/utils/filters.js
+++ scripts/utils/filters.js
@@ -1,4 +1,23 @@
-const filterBtn = document.querySelectorAll('.search__filters__filter__wrapper__input__btn');
-const filterList = document.querySelectorAll('.search__filters__filter__wrapper__list');
-const filter = document.querySelectorAll('.search__filters__filter');
-const filterName = document.querySelectorAll('.search__filters__filter__wrapper__input__label')
+class Filter {
+
+    constructor(filter) {
+        this.name = filter.dataset.filter;
+        this.container = filter;
+        this.label = filter.querySelector('[data-filter-label]');
+        this.input = filter.querySelector('[data-filter-input]');
+        this.list = filter.querySelector('[data-filter-list]');
+        this.control = filter.querySelector('[data-filter-control]');
+    }
+};
+
+class ListElements {
+    constructor(name) {
+        this.name = name;
+    }
+
+    displayItem() {
+        const listElem = document.createElement('li');
+        listElem.innerText = this.name;
+        return listElem;
+    }
+}
